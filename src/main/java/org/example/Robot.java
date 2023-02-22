1181026b61869e67e187b690a607bdbae5542bef
Mark S <stcoronawraps@gmail.com>
2023-02-22 15:24:16 -0700

    Added Robot class + named robot-one

---
+++ src/main/java/org/example/Robot.java
@@ -0,0 +1,18 @@
+package org.example;
+
+import java.sql.SQLOutput;
+
+public class Robot {
+    //Instance variables (HAS A)
+    public String name;
+    public String weaponName;
+    public int weaponPower;
+    //Constructor
+    public Robot() {
+        this.name = "Earl";
+        this.weaponName = "Hammer";
+        this.weaponPower = 50;
+    }
+
+
+}
